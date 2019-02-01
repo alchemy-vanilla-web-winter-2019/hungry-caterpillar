9e68ccb63e2c40b9041747752df2784c1efc6294
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-31 19:32:09 -0800

    Add circles corresponding to the fruit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const feedButtons = document.getElementById('feed-buttons');
+const bugBody = document.getElementById('bug-body');
+const danceButtons = document.getElementById('dance-buttons');
 const fruitArray = ['Apple', 'Pear', 'Lemon', 'Grape', 'Orange', 'Blueberry'];
+
+function feed(fruit) {
+    const section = document.createElement('span');
+    bugBody.appendChild(section);
+    section.classList.add('section', fruit);
+}
 
 for(let i = 0; i < fruitArray.length; i++) {
     const fruit = fruitArray[i];
@@ -9,4 +17,7 @@ for(let i = 0; i < fruitArray.length; i++) {
     feedButtons.appendChild(fruitButton);
     fruitButton.classList.add('fruit-button', fruit);
 
+    fruitButton.addEventListener('click', function() {
+        feed(fruit);
+    });
 }
