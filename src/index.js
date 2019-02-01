a679efaf0a6cbff0958d09b2fe08b897d5511825
Ben Parisot <me@benparisot.com>
2019-01-31 22:19:28 -0800

    struggle to squash bug that won't allow for correct dance position

--- src/index.js
+++ src/index.js
@@ -2,35 +2,57 @@ const fruitButtonsNode = document.getElementById('fruit-buttons');
 const canvasNode = document.getElementById('canvas');
 const danceButtonsNode = document.getElementById('dance-buttons');
 
-const fruits = [
-    'apple',
-    'kiwi',
-    'banana',
-    'cherry'
-];
+const fruits = ['apple', 'kiwi', 'banana', 'cherry'];
 
 for(let index = 0; index < fruits.length; index++) {
-    const fruit = fruits[index];
-    const fruitNode = document.createElement('button');
+    let fruit = fruits[index];
+    const fruitButtonNode = document.createElement('button');
 
-    fruitNode.value = fruit;
-    fruitNode.textContent = fruit;
-    fruitNode.classList.add(fruit, 'fruit-button');
-    fruitNode.id = fruit;
+    fruitButtonNode.value = fruit;
+    fruitButtonNode.textContent = fruit;
+    fruitButtonNode.classList.add(fruit, 'fruit-button');
 
-    fruitNode.addEventListener('click', function(){
+    fruitButtonNode.addEventListener('click', function(){
         paint(fruit);
     });
 
-    fruitButtonsNode.appendChild(fruitNode);
-
+    fruitButtonsNode.appendChild(fruitButtonNode);
 }
+
 
 function paint(fruit) {
     const bodySegment = document.createElement('span');
-
-    bodySegment.classList.add(fruit, 'fruit-button');
+    bodySegment.classList.add('body-segment', fruit);
 
     canvasNode.appendChild(bodySegment);
     console.log(bodySegment);
+}
+
+for(let index = 0; index < fruits.length; index++) {
+    let fruit = fruits[index];
+    const danceButtonNode = document.createElement('button');
+
+    danceButtonNode.value = fruit;
+    danceButtonNode.textContent = fruit;
+    danceButtonNode.classList.add(fruit, 'dance-button');
+
+    danceButtonNode.addEventListener('click', function(){
+        dance(fruit);
+    });
+
+    danceButtonsNode.appendChild(danceButtonNode);
+}
+
+function dance(fruit) {
+    const selector = '.body-segment.' + fruit;
+    const bodySegments = document.querySelectorAll(selector);
+    const allBodySegments = document.querySelectorAll('.body-segment');
+
+    for(let index = 0; index < allBodySegments.length; index++) {
+        allBodySegments[index].classList.remove('dance-segment');
+    }
+
+    for(let index = 0; index < bodySegments.length; index++) {
+        bodySegments[index].classList.add('dance-segment');
+    }
 }
