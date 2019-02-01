41cbcb33b4ee55adaf3f4ef7b877ab5c07868fc5
Heather <unphurlng@gmail.com>
2019-02-01 10:25:28 -0800

    for loop grabbing each fruit from aray

---
+++ src/index.js
@@ -0,0 +1,19 @@
+// create array
+const fruits = ['strawberry', 'orange', 'banana', 'pear', 'blueberry', 'grape'];
+
+// connecting to feed me section
+const fruitButtons = document.getElementById('fruit-buttons');
+
+// for loop
+for(let i = 0; i < fruits.length; i++) {
+    // create variable for each fruit
+    const fruit = fruits[i];
+    console.log(fruit);
+
+    // iterate over each fruit to ...
+        // create a button
+        // assign classes
+        // have text show up
+        // 
+}
+
