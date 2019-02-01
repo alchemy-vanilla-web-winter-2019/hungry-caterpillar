60898ab6ae02f354d8d2918048dc970000c5190c
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-01-31 23:08:13 -0800

    working on functions and loops

--- src/index.js
+++ src/index.js
@@ -14,14 +14,14 @@ for(let index = 0; index < colors.length; index++) {
     const colorButtonNode = document.createElement('button'); //colorButtonNode is a new variable and we created a button element that it corresponds to
     colorButtonNode.textContent = color; //need to change this to image
     colorButtonNode.value = color; //the color buttons will have a value of each color
-    colorButtonNode.classList.add('color-button', color); //adding the 'color-button' and color class to the colorButtonNodes
+    colorButtonNode.classList.add(color, 'color-button'); //adding the 'color-button' and color class to the colorButtonNodes
 
-    colorButtonNode.addEventListener('submit', function() {
+    colorButtonNode.addEventListener('click', function() {
         paint(color);
     }); 
     
-    console.log(colorButtonNode);
     colorButtonsNode.appendChild(colorButtonNode);
+    console.log(paint);
 }
 
 function paint(color) {
@@ -37,11 +37,24 @@ for(let index = 0; index < colors.length; index++) {
     transformButtonNode.value = color; //the color buttons will have a value of each color
     transformButtonNode.classList.add('transform-button', color); //adding the 'color-button' and color class to the colorButtonNodes
                     
-    transformButtonNode.addEventListener('submit', function() {
+    transformButtonNode.addEventListener('click', function() {
         dance(color);
     });                   
     console.log(transformNode);
     transformNode.appendChild(transformButtonNode);
 }
 
+function dance(color) {
+    const selector = '.body-segment' + color;
+    const bodySegments = document.querySelectorAll(selector);
+    const allBodySegments = document.querySelectorAll('.body-segment');
+
+    for(let index = 0; index < allBodySegments.length; index++) {
+        allBodySegments[index].classList.remove('dance-segment');
+    }
+    for(let index = 0; index < bodySegments.length; index++) {
+        bodySegments[index].classList.add('dance-segment');
+
+    }
+}
 
