0123d92a62592d6e37def973a26666823cfd61c2
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-02-01 15:22:33 -0700

    4 fruit buttons working, adding colors to caterpillar body when clicked

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 const caterContainer = document.getElementById('caterpillar-container');
 const buttonContainer = document.getElementById('feed-button-container');
-const feedButton = document.createElement('button');
 
-buttonContainer.appendChild(feedButton);
-feedButton.textContent = 'FEED ME';
-
-feedButton.addEventListener('click', function() {
-    const caterBodySpan = document.createElement('span');
-    caterContainer.appendChild(caterBodySpan);
-    caterBodySpan.classList.add('green');
+const fruits = ['plum', 'fig', 'strawberry', 'blackberry'];
 
 
 
-});
+for(let index = 0; index < fruits.length; index++) {
+    const fruit = fruits[index];
+    const feedButton = document.createElement('button');
+    buttonContainer.appendChild(feedButton);
+    feedButton.textContent = fruit;
+
+    feedButton.addEventListener('click', function() {
+        const caterBodySpan = document.createElement('span');
+        caterContainer.appendChild(caterBodySpan);
+        caterBodySpan.classList.add(fruit + '-span', 'create-span');
+
+
+    });
+}
+
+
+
