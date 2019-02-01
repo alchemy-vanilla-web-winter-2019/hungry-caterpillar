8f66700d8a8b8e739cb6af565ad2941804e4bd10
Leslie Shea <leslieyshea@gmail.com>
2019-01-31 17:58:14 -0800

    wrote for loop for grabbing colors from array and assigning to button

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// grab parent nodes from html
+// console.log after grabbing the node to make sure it actually grabs it
+const colorButtonsNode = document.getElementById('color-buttons');
+const canvasNode = document.getElementById('canvas');
+
+const colors = [
+    'red',
+    'blue',
+    'green',
+    'yellow'
+];
+
+// create button elements
+// goal is to loop through array, and assign text (textContent), class (class ids), and value to the button
+// append button to parent node
+for(let index = 0; index < colors.length; index++) {
+    let color = colors[index];
+    const colorButtonNode = document.createElement('button');
+    colorButtonNode.textContent = color;
+    colorButtonNode.value = color;
+    colorButtonNode.classList.add('color-button', color);
+
+    colorButtonNode.addEventListener('click', function() {
+        //paint(color);
+    });
+    colorButtonsNode.appendChild(colorButtonNode);
+}
+
+
+
+
+
