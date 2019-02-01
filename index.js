8ad4eeff8c48f402fbcc01dd8cd83e64aa1f88e3
Cheri Portman <cheriportman@hotmail.com>
2019-02-01 10:38:46 -0800

    dynamic meal buttons populate and register click

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
-meals = ['pig', 'cow', 'maiden'];
+const meals = ['pig', 'cow', 'maiden'];
 
-//make a button
+const mealButtons = document.getElementById('meal-buttons');
+
+//loop through array 
+    //make button
+    //add value to the meal
+    //add name to the meal
+    //add classes to the buttons
+    //.pig, .cow, etc
+    // append the buttons to the dom in the 'buttons' section 
+    //add event listener to the button
+
+for(let i = 0; i < meals.length; i++) {
+    const meal = meals[i];                      //grabbing individual meal by index
+    const mealButton = document.createElement('button');  //make the button
+
+    mealButton.value = meal;        //add value to the button
+
+    mealButton.textContent = meal; //add a name to thd button
+
+    mealButton.classList.add('meal-button', meal); //add class to the button
+
+    mealButtons.appendChild(mealButton);
+
+    mealButton.addEventListener('click', function() {
+        console.log('clicked', mealButton);
+    });
+
+
+
+}
