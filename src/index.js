5499e0e87b2ea377b87b5d5998775cc5e5cd2798
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-31 17:59:28 -0800

    end of day

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 const feedButtons = document.getElementById('feed-buttons');
-console.log(feedButtons);
+const fruitArray = ['Apple', 'Pear', 'Lemon', 'Grape', 'Orange', 'Blueberry'];
+
+for(let i = 0; i < fruitArray.length; i++) {
+    const fruit = fruitArray[i];
+    const fruitButton = document.createElement('button');
+    fruitButton.value = fruit;
+    fruitButton.textContent = fruit;
+    feedButtons.appendChild(fruitButton);
+    fruitButton.classList.add('fruit-button', fruit);
+
+}
