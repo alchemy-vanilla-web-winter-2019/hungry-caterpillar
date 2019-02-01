0296c733f8a6c095040cfad412ff87730849cf00
Cheri Portman <cheriportman@hotmail.com>
2019-02-01 12:05:31 -0800

    scales populate upon click

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-const meals = ['pig', 'cow', 'youth', 'warrior'];
+const meals = ['chicken', 'pig', 'cow', 'youth', 'warrior'];
+
 const mealButtons = document.getElementById('meal-buttons');
+const scales = document.getElementById('scales');
+
+function makeScale(meal) {
+    const scale = document.createElement('span');
+    scale.classList.add('scale', meal);
+    scales.appendChild(scale);
+}
+
 
 for(let i = 0; i < meals.length; i++) {
-    const meal = meals[i];
-    
-    //make the damned button
+    const meal = meals[i];   
     const mealButton = document.createElement('button');
+    
     mealButton.value = meal;
     mealButton.textContent = meal;
     mealButton.classList.add('meal-buttons', meal);
@@ -13,5 +21,6 @@ for(let i = 0; i < meals.length; i++) {
 
     mealButton.addEventListener('click', function() {
         console.log('clicked', meal);
+        makeScale(meal);
     });
 }
