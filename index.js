43d20a06af7f76f5ae659af8109bbe971840cdf9
Cheri Portman <cheriportman@hotmail.com>
2019-02-01 11:21:39 -0800

    rewrote meal for-loop three times from scratch for brain development"

--- index.js
+++ index.js
@@ -1,32 +1,17 @@
-const meals = ['pig', 'cow', 'maiden'];
-
+const meals = ['pig', 'cow', 'youth', 'warrior'];
 const mealButtons = document.getElementById('meal-buttons');
 
-//loop through array 
-    //make button
-    //add value to the meal
-    //add name to the meal
-    //add classes to the buttons
-    //.pig, .cow, etc
-    // append the buttons to the dom in the 'buttons' section 
-    //add event listener to the button
-
 for(let i = 0; i < meals.length; i++) {
-    const meal = meals[i];                      //grabbing individual meal by index
-    const mealButton = document.createElement('button');  //make the button
-
-    mealButton.value = meal;        //add value to the button
-
-    mealButton.textContent = meal; //add a name to thd button
-
-    mealButton.classList.add('meal-button', meal); //add class to the button
-
+    const meal = meals[i];
+    
+    //make the damned button
+    const mealButton = document.createElement('button');
+    mealButton.value = meal;
+    mealButton.textContent = meal;
+    mealButton.classList.add('meal-buttons', meal);
     mealButtons.appendChild(mealButton);
 
     mealButton.addEventListener('click', function() {
-        console.log('clicked', mealButton);
+        console.log('clicked', meal);
     });
-
-
-
 }
