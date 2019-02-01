61db117d716dfed8ce31ed6c99b5801f749babe6
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-01-31 22:30:31 -0800

    added images and more js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const colorButtonsNode = document.getElementById('color-buttons');
+const canvasNode = document.getElementById('canvas');
+const transformNode = document.getElementById('transform-buttons');
 
 const colors = [
     'red',
@@ -14,10 +16,32 @@ for(let index = 0; index < colors.length; index++) {
     colorButtonNode.value = color; //the color buttons will have a value of each color
     colorButtonNode.classList.add('color-button', color); //adding the 'color-button' and color class to the colorButtonNodes
 
-    colorButtonNode.addEventListenter('submit', function() {
+    colorButtonNode.addEventListener('submit', function() {
         paint(color);
-    }) 
+    }); 
     
-console.log(colorButtonNode);
+    console.log(colorButtonNode);
+    colorButtonsNode.appendChild(colorButtonNode);
 }
+
+function paint(color) {
+    const bodySegment = document.createElement('span');
+    bodySegment.classList.add('body-segment', color);
+    canvasNode.appendChild(bodySegment);
+}
+
+for(let index = 0; index < colors.length; index++) {
+    let color = colors[index]; //the color array index = color
+    const transformButtonNode = document.createElement('button'); //transormNode is a new variable and we created a button element that it corresponds to
+    transformButtonNode.textContent = color; //need to change this to image
+    transformButtonNode.value = color; //the color buttons will have a value of each color
+    transformButtonNode.classList.add('transform-button', color); //adding the 'color-button' and color class to the colorButtonNodes
+                    
+    transformButtonNode.addEventListener('submit', function() {
+        dance(color);
+    });                   
+    console.log(transformNode);
+    transformNode.appendChild(transformButtonNode);
+}
+
 
