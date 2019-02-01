057db6b68e7f6d58c6d47194c5a3df670ac7c976
meganamarshall <MEGAN.ALYS.MARSHALL@GMAIL.COM>
2019-02-01 08:22:02 -0800

    finished with functionality

--- src/index.js
+++ src/index.js
@@ -9,18 +9,30 @@ const colors = [
     'blue',
 ];
 
+const imgSrc = [
+    '../assets/redfish.png',
+    '../assets/pizza.png',
+    '../assets/cheese.png',
+    '../assets/bluefish.png',
+];
+
 for(let index = 0; index < colors.length; index++) {
     let color = colors[index]; //the color array index = color
     const colorButtonNode = document.createElement('button'); //colorButtonNode is a new variable and we created a button element that it corresponds to
-    colorButtonNode.textContent = color; //need to change this to image
+    const imgNode = document.createElement('img'); //creating the images in html and assigning them the imgNode variable
+    imgNode.src = imgSrc[index];
     colorButtonNode.value = color; //the color buttons will have a value of each color
     colorButtonNode.classList.add(color, 'color-button'); //adding the 'color-button' and color class to the colorButtonNodes
 
     colorButtonNode.addEventListener('click', function() {
-        paint(color);
+        const segmentCount = document.querySelectorAll('.body-segment').length;
+        if(segmentCount < 5) {
+            paint(color);
+        }
     }); 
     
     colorButtonsNode.appendChild(colorButtonNode);
+    colorButtonNode.appendChild(imgNode);
     console.log(paint);
 }
 
@@ -39,22 +51,22 @@ for(let index = 0; index < colors.length; index++) {
                     
     transformButtonNode.addEventListener('click', function() {
         dance(color);
-        console.log(color);
     });                   
     transformNode.appendChild(transformButtonNode);
 }
 
 function dance(color) {
-    const selector = '.body-segment' + color;
-    const bodySegments = document.querySelectorAll(selector);
-    const allBodySegments = document.querySelectorAll('.body-segment');
+    const selector = '.body-segment.' + color; //selecting by body segment and color
+    const bodySegments = document.querySelectorAll(selector);//setting bodySegments as a variable and equating it to the selector I just created
+    const allBodySegments = document.querySelectorAll('.body-segment'); //setting allBodySegments as a selector and equating it to the body-segment class (not just color - this will move all of them down when transform buttons are pressed)
 
     for(let index = 0; index < allBodySegments.length; index++) {
         allBodySegments[index].classList.remove('dance-segment');
     }
     for(let index = 0; index < bodySegments.length; index++) {
         bodySegments[index].classList.add('dance-segment');
-
     }
+
+    
 }
 
