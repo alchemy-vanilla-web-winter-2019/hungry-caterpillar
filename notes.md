652e0ac6fa0550d81d0ac6ef539cd7511511c5ac
Heather <unphurlng@gmail.com>
2019-02-01 15:19:47 -0800

    create paintUniKitty function createElement

--- notes.md
+++ notes.md
@@ -22,11 +22,13 @@
     + add text to fruit button
     + append fruit button to section for fruit Buttons
     + add classes to each fruit button
-    + add event listener
-    + function
+    + add event listener that calls paintUniKitty function
 
 3. function #1
-    + add segment
+    + create paintUniKitty function that takes color
+    + creates span element
+    + adds color class to element
+    + appends element to the DOM parent (appendChild)
 
 4. for loop #2
     + create variable (singular) set to iterate fruits in array
--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
 const colorButtons = document.getElementById('color-buttons');
+const uniKittyBody = document.getElementById('uniKitty')
+
+//function that takes color
+//creates a span element
+//adds color class to that element
+//appends that element to the dom parent (appendchild)
+
+function paintUniKitty() {
+    const uniKittyBody = document.createElement('span');
+    
+    // uniKittyBody.classList.add('color-segment', color);
+    // uniKittyBody.appendChild()
+}
 
 for(let i = 0; i < colors.length; i++) {
     const color = colors[i];
@@ -8,8 +21,8 @@ for(let i = 0; i < colors.length; i++) {
     colorButtons.appendChild(colorButton);
     colorButton.classList.add('color-button', color);
 
-    colorButton.addEventListener('click', function(event) {
-        
+    colorButton.addEventListener('click', function() {
+        paintUniKitty(color);
     });
 
     // iterate over each color to ...
@@ -19,10 +32,11 @@ for(let i = 0; i < colors.length; i++) {
         // assign classes - done    
         // eventListener with function to add segment
 }
-
+console.log('body span', uniKittyBody);
 for(let i = 0; i < colors.length; i++) {
     const color = colors[i];
     console
+
     // iterate over each color to ...
         // create a button element
         // have text show up
