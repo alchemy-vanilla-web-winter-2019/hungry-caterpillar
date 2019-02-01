64c7406672f3279d3478e9f31a5ffbb07313b23e
Chris Piccaro <chrispiccaro18@gmail.com>
2019-01-31 20:00:05 -0800

    Dancing

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ function feed(fruit) {
     section.classList.add('section', fruit);
 }
 
+function dance(fruit) {
+    const dancing = document.querySelectorAll('.dance');
+    dancing.forEach(function(element) {
+        element.classList.remove('dance');
+    });
+    
+    const section = '.section.' + fruit;
+    const selected = document.querySelectorAll(section);
+    selected.forEach(function(element) {
+        element.classList.add('dance');
+    });
+}
+
 for(let i = 0; i < fruitArray.length; i++) {
     const fruit = fruitArray[i];
     const fruitButton = document.createElement('button');
@@ -19,5 +32,18 @@ for(let i = 0; i < fruitArray.length; i++) {
 
     fruitButton.addEventListener('click', function() {
         feed(fruit);
+    });
+}
+
+for(let i = 0; i < fruitArray.length; i++) {
+    const fruit = fruitArray[i];
+    const danceButton = document.createElement('button');
+    danceButton.value = fruit;
+    danceButton.textContent = fruit;
+    danceButtons.appendChild(danceButton);
+    danceButton.classList.add('dance-button', fruit);
+
+    danceButton.addEventListener('click', function() {
+        dance(fruit);
     });
 }
