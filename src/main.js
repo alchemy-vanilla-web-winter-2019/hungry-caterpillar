c3343ee0fbf45a03cebe1977b3438a93dadeac65
mcnadams <mcneilbo@oregonstate.edu>
2019-02-01 09:46:13 -0800

    add dance buttons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const fruitButtonNode = document.getElementById('fruit-buttons');
 const canvas = document.getElementById('canvas');
+const danceButtonNode = document.getElementById('dance-buttons');
 
 
 const colors = [
@@ -16,6 +17,7 @@ const fruitImgs = [
     '../assets/grapes.png'
 ];
 
+//make fruit buttons
 for(let i = 0; i < colors.length; i++) {
     const color = colors[i];
     const fruitButton = document.createElement('button');
@@ -39,5 +41,34 @@ function growCaterpillar(color) {
         const bodySegment = document.createElement('span');
         bodySegment.classList.add('body-segment', color);
         canvas.appendChild(bodySegment);
+    }
+}
+
+//make dance buttons
+for(let i = 0; i < colors.length; i++) {
+    const color = colors[i];
+    const danceButton = document.createElement('button');
+    danceButton.value = color;
+    danceButton.textContent = color;
+    danceButton.classList.add(color);
+
+
+    danceButton.addEventListener('click', function() {
+        dance(color);
+    });
+
+    danceButtonNode.appendChild(danceButton);
+}
+
+function dance(color) {
+    const danceSegments = document.querySelectorAll('.body-segment.' + color);
+    const allSegments = document.querySelectorAll('.body-segment');
+
+    for(let i = 0; i < allSegments.length; i++) {
+        allSegments[i].classList.remove('dance');
+    }
+
+    for(let i = 0; i < danceSegments.length; i++) {
+        danceSegments[i].classList.add('dance');
     }
 }
