d13da2a48650d908e653414220d228ae667b4311
bpkotowski <bighoopee@gmail.com>
2019-02-01 16:57:36 -0800

    worm wiggles now

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const colors = [
 
 const canvasNode = document.getElementById('canvas');
 const wiggleNode = document.getElementById('wiggle-button');
-
+// creates the buttons to feed the worm
 for(let index = 0; index < colors.length; index++) {
     let color = colors[index];
     const meatButtonNode = document.createElement('button');
@@ -19,23 +19,27 @@ for(let index = 0; index < colors.length; index++) {
     meatButtonNode.classList.add('meat-button');
    
     meatButtonNode.addEventListener('click', function() {
+        // const bodyLength = document.querySelectorAll('dance-segment').length;
+        // if(bodyLength < 6) {
+        //     paint(color);
+        // }
         paint(color); // add const query body length?
     });
 
     meatNodes.appendChild(meatButtonNode);
         
-    
+     
 } 
-
+// creates the body segments when feeding the worm
 function paint(color) {
     const colorBlock = document.createElement('span');
     colorBlock.classList.add(color);
-    colorBlock.classList.add('color-block', color); // missing ,color?
+    colorBlock.classList.add('color-block', color); 
     canvasNode.appendChild(colorBlock);
     
 
 }
-
+// creates the the buttons to wiggle the worm
 for(let index = 0; index < colors.length; index++) {
     
     let color = colors[index];
@@ -43,7 +47,7 @@ for(let index = 0; index < colors.length; index++) {
     const wiggleNodeButton = document.createElement('button');
     wiggleNodeButton.textContent = color;
     wiggleNodeButton.value = color;
-    wiggleNodeButton.classList.add(color, 'wiggle-button');
+    wiggleNodeButton.classList.add(color, 'color-block');//
 
     wiggleNodeButton.addEventListener('click', function() {
         wiggle(color);
@@ -53,19 +57,22 @@ for(let index = 0; index < colors.length; index++) {
     wiggleNode.appendChild(wiggleNodeButton);
     
 }
-
+// function to wiggle the worm
 function wiggle(color) {
-    const selector = 'wiggle-button' + color;
-    const body = document.querySelectorAll(selector);
-    const allBody = document.querySelectorAll('.wiggle-button');
-
-    for(let index = 0; index < allBody.length; index++) {
-        allBody[index].classList.remove('wiggle-button');
+    const selector = '.' + color;
+    const bodySegment = document.querySelectorAll(selector);
+  
+    const allBodySegments = document.querySelectorAll('.color-block');
+console.log(allBodySegments);
+    for(let index = 0; index < allBodySegments.length; index++) {
+        allBodySegments[index].classList.remove('dance-segment');
     }
 
-    for(let index = 0; index < body.length; index++) {
-        allBody[index].classList.add('wiggle-button');
+    for(let index = 0; index < bodySegment.length; index++) {
+        bodySegment[index].classList.add('dance-segment');
     }
+
+    
     
 }
 
