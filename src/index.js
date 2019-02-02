e645ab99733f82bf06a6c0bba34c8aee80bdc137
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-02-01 18:12:43 -0700

    trying to make it animated cant look at the screen any longer

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 const caterContainer = document.getElementById('caterpillar-container');
 const buttonContainer = document.getElementById('feed-button-container');
+const danceButtonContainer = document.getElementById('dance-button-container');
 
 const fruits = ['plum', 'fig', 'strawberry', 'blackberry'];
-
-
 
 for(let index = 0; index < fruits.length; index++) {
     const fruit = fruits[index];
@@ -14,11 +13,32 @@ for(let index = 0; index < fruits.length; index++) {
     feedButton.addEventListener('click', function() {
         const caterBodySpan = document.createElement('span');
         caterContainer.appendChild(caterBodySpan);
-        caterBodySpan.classList.add(fruit + '-span', 'create-span');
-
-
+        caterBodySpan.classList.add(fruit + '-span', 'create-span'); 
     });
+    
 }
+
+
+const danceButton = document.createElement('button');
+danceButtonContainer.appendChild(danceButton);
+danceButton.textContent = 'dance!!';
+
+
+
+danceButton.addEventListener('click', function() {
+    const spanArray = document.querySelectorAll('.create-span');
+
+    for(let index = 0; index < spanArray.length; index += 2) {
+        spanArray[index].classList.add('make-dance');
+        setTimeout(function() {
+            spanArray[index].classList.remove('make-dance');
+
+        }, 1000);
+    }
+
+});
+
+
 
 
 
