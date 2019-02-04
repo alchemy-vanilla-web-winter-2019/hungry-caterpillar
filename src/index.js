3f491539201b72bcbac0de1c6ba608d5d1ae8519
bpkotowski <bighoopee@gmail.com>
2019-02-03 19:37:39 -0800

    broke it, fixed it, broke and fixed it again

--- src/index.js
+++ src/index.js
@@ -1,56 +1,85 @@
-const meatNodes = document.getElementById('meat-buttons');
+const meatButtonSSNode = document.getElementById('meat-buttons');
 
-const colors = [
-    'black',
-    'red',
-    'yellow',
-    'white'
-]; 
+// const colors = [
+//     'tomato',
+//     'indianred',
+//     'pink',
+//     'palevioletred',
+//     'plum'
+// ]; 
+
+const meats = [
+    'brain', 
+    'chicken', 
+    'sausage', 
+    'shrimp', 
+    'steak'
+];
+
+
+const imgSrc = [ 
+    '../assets/brain.png',
+    '../assets/chicken.png',
+    '../assets/shrimp.jpg',
+    '../assets/sausage.jpg',
+    '../assets/steak.png',
+    '../assets/Zmaggot-head.png/'
+];
 
 const canvasNode = document.getElementById('canvas');
 const wiggleNode = document.getElementById('wiggle-button');
+
+
 // creates the buttons to feed the worm
-for(let index = 0; index < colors.length; index++) {
-    let color = colors[index];
+for(let index = 0; index < meats.length; index++) {
+    let meat = meats[index];
     const meatButtonNode = document.createElement('button');
+    //create array of images
+    const imgNode = document.createElement('img');
+    imgNode.src = imgSrc[index];
     
-    meatButtonNode.textContent = color;
-    meatButtonNode.value = color;
+    meatButtonNode.textContent = meat;
+    meatButtonNode.value = meat;
     meatButtonNode.classList.add('meat-button');
    
     meatButtonNode.addEventListener('click', function() {
-        // const bodyLength = document.querySelectorAll('dance-segment').length;
-        // if(bodyLength < 6) {
-        //     paint(color);
-        // }
-        paint(color); // add const query body length?
+        const bodyLength = document.querySelectorAll('.body-length').length;
+        if(bodyLength < 5) {
+            paint(meat);
+        }
+        
     });
 
-    meatNodes.appendChild(meatButtonNode);
+    meatButtonNode.appendChild(imgNode);
+    meatButtonSSNode.appendChild(meatButtonNode);
         
      
 } 
 // creates the body segments when feeding the worm
-function paint(color) {
-    const colorBlock = document.createElement('span');
-    colorBlock.classList.add(color);
-    colorBlock.classList.add('color-block', color); 
-    canvasNode.appendChild(colorBlock);
+function paint(meat) {
+    const bodyLength = document.createElement('span');
+    bodyLength.classList.add('body-length', meat);
+
+
+    bodyLength.classList.add(meat);
+    bodyLength.classList.add('body-length', meat); 
+    canvasNode.appendChild(bodyLength);
     
 
 }
 // creates the the buttons to wiggle the worm
-for(let index = 0; index < colors.length; index++) {
+for(let index = 0; index < meats.length; index++) {
     
-    let color = colors[index];
+    let meat = meats[index];
+    
 
     const wiggleNodeButton = document.createElement('button');
-    wiggleNodeButton.textContent = color;
-    wiggleNodeButton.value = color;
-    wiggleNodeButton.classList.add(color, 'wiggle-button');//
+    wiggleNodeButton.textContent = meat;
+    wiggleNodeButton.value = meat;
+    wiggleNodeButton.classList.add(meat, 'wiggle-button');//
 
     wiggleNodeButton.addEventListener('click', function() {
-        wiggle(color);
+        wiggle(meat);
     
     });
     
@@ -58,11 +87,12 @@ for(let index = 0; index < colors.length; index++) {
     
 }
 // function to wiggle the worm
-function wiggle(color) {
-    const selector = '.' + color;
+function wiggle(meat) {
+    const selector = '.body-length.' + meat;// it was a goddamn'.'!!!
     const bodySegment = document.querySelectorAll(selector);
+    console.log(bodySegment);
   
-    const allBodySegments = document.querySelectorAll('.color-block');
+    const allBodySegments = document.querySelectorAll('.body-length');
 
     for(let index = 0; index < allBodySegments.length; index++) {
         allBodySegments[index].classList.remove('dance-segment');
